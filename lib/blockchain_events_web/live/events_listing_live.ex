37ce4ea8e64222bc51628df062e7acda94105ec0
Sierra <iamsi2@protonmail.com>
2020-05-13 09:42:55 +0300

    add card labels

--- lib/blockchain_events_web/live/events_listing_live.ex
+++ lib/blockchain_events_web/live/events_listing_live.ex
@@ -6,7 +6,44 @@ defmodule BlockchainEventsWeb.EventsListingLive do
   alias GSS.DataSync
 
   def mount(_params, _session, socket) do
-    ss_data = DataSync.read_rows()
+    ss_data = [
+      %GSS.Data{
+        blockchain: "jhjh",
+        date: ~D[2020-12-24],
+        dev_type: ["sdgdg"],
+        event_name: "n",
+        event_type: "course",
+        id: 1,
+        organizers: ["sgsgd"],
+        price: true,
+        prices: "afsfa",
+        prize: true,
+        prizes_description: "afsf",
+        rounds_and_dates: "afsaf",
+        summary: nil,
+        technologies: ["dgdg"],
+        urls: ["afsafasf"]
+      },
+      %GSS.Data{
+        blockchain: "sdfsdf",
+        date: ~D[2020-12-25],
+        dev_type: ["wfxc"],
+        event_name: "dsd",
+        event_type: "seminar",
+        id: 2,
+        organizers: ["xvxcv"],
+        price: nil,
+        prices: "asd",
+        prize: true,
+        prizes_description: "xcv c",
+        rounds_and_dates: "sdf vx",
+        summary: nil,
+        technologies: ["svcxvv"],
+        urls: [""]
+      }
+    ]
+
+    # DataSync.read_rows()
 
     socket =
       socket
